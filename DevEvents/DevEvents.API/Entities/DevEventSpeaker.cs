4f6c0001eff3f3927316605c33464b3991744b63
Caio_lima <115504991+Caosezar@users.noreply.github.com>
2023-10-04 21:26:16 -0300

    Add files via upload

---
+++ DevEvents/DevEvents.API/Entities/DevEventSpeaker.cs
@@ -0,0 +1,12 @@
+﻿namespace DevEvents.API.Entities
+{
+    public class DevEventSpeaker
+    {
+        public Guid Id{ get; set; }
+        public string Nome{ get; set; }
+        public string TalkTitle { get; set; }
+        public string TalkDescription { get; set; }
+        public string LinkedInProfile { get; set;}
+
+    }
+}
---
+++ DevEvents/DevEvents.API/Persistence/DevItensDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DevEvents.API.Entities;
+
+namespace DevEvents.API.Persistence
+{
+    public class DevItensDbContext
+    {
+        public List<DevEvent> DevEvents {get; set;}
+
+        public DevItensDbContext()
+        {
+            DevEvents = new List<DevEvent>();
+        }
+    }
+}
